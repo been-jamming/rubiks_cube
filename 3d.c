36570294df7a00fbef4de5e8aafa02bdda2fdd43
unknown <computergeek3.142@gmail.com>
2020-02-09 04:55:01 -0700

    Improving menu selection visibility on consoles with dark blue colors

--- 3d.c
+++ 3d.c
@@ -595,7 +595,7 @@ int main(int argc, char **argv){
 	keypad(stdscr, 1);
 	start_color();
 	init_pair(1, COLOR_WHITE, COLOR_BLACK);
-	init_pair(2, COLOR_BLACK, COLOR_BLUE);
+	init_pair(2, COLOR_BLACK, COLOR_CYAN);
 	CAM_init(3);
 
 	if(create_z_buffer()){
